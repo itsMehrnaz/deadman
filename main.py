ccea222317b1d97f554c69c1c3407c64e86d456b
itsMehrnaz <ItsmeMehrnaz@gmail.com>
2026-07-25 10:18:17 +0330

    update main

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import requests
+import jdatetime
+import subprocess
+import time
 
 TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IntcIlVzZXJJcFwiOm51bGwsXCJVc2VySWRcIjoxNjMzMzQ3NyxcIlNlc3Npb25LZXlcIjpudWxsfSIsImV4cCI6MTc5OTk3MTUzNywiaWF0IjoxNzg0MDczOTM3LCJuYmYiOjE3ODQwNzM5Mzd9.1sP_FB2FfkfPUVAcax8bUdKdyVd5-80E3j13NUMCC7I"  
 KEYWORD = "بعثت 10"
@@ -25,3 +28,42 @@ def get_outages(bill_id, from_date, to_date):
 outages = get_outages("1465176505229", "1405/04/24", "1405/04/29")
 
 print(outages)
+
+
+
+def check_shutdown(outages):
+    now = jdatetime.datetime.now()
+    today = jdatetime.date.today()
+    for outage in outages:
+        if (str(outage["outage_date"]) == str(today)):
+                now_minuts = int(now.hour * 60 + now.minute)
+                start_time = outage["outage_time"]
+                start_time_minutes = start_time.split(":")
+                start_time = int(start_time_minutes[0]) * 60 + int(start_time_minutes[1])
+                print(now_minuts)   
+                print(start_time)   
+                diff = start_time - now_minuts
+                print(diff) 
+                if (diff <= 10 and diff > 0):
+                     print("shutdown")
+                     ###subprocess.run(["shutdown", "-h", "now"])
+
+    return "ok"
+
+check_shutdown(outages)
+
+
+i = 0
+
+while True:
+     check_shutdown(outages)
+     time.sleep(60)
+
+     i = i + 1
+
+     if i == 60:
+          outages = get_outages("1465176505229", "1405/04/24", "1405/04/29")
+          i = 0
+          print("still working")     
+    
+     
